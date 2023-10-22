992aa53734b01d95e18790569fa9b8a48fc79df3
soandra <soandradep@unal.edu.co>
2023-10-22 00:25:44 -0500

    Data base created

---
+++ app_de_consola_con_mysql/database.sql
@@ -0,0 +1,20 @@
+CREATE TABLE users(
+    id        int(25) auto_increment not null,
+    name    varchar(100),
+    last_name varchar(255),
+    email     varchar(255) not null,
+    password  varchar(255) not null,
+    date  date not null,
+    CONSTRAINT  PK_users PRIMARY KEY(id),
+    CONSTRAINT  uq_email UNIQUE(email)
+)ENGINE=InnoDb;
+
+CREATE TABLE notes(
+    id int(25) auto_increment,
+    user_id int(25) not null,
+    title varchar(255) not null,
+    description MEDIUMTEXT, 
+    date  date not null, 
+    CONSTRAINT pk_notes PRIMARY KEY(id),
+    CONSTRAINT fk_user_note FOREIGN KEY(user_id) REFERENCES users(id)
+)ENGINE=InnoDb;
---
+++ app_de_consola_con_mysql/main.py
@@ -0,0 +1,9 @@
+"""
+Proyecto Python y MySQL:
+- Abrir asistente
+- Login o registro
+- Si elegimo registro, creará un usario en la base de datos
+- Si elegimos login, identificara al usuario y preguntará
+    Crear notam mostrar nota, borrar nota.
+
+"""
